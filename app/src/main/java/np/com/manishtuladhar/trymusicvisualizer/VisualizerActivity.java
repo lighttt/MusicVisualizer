e956c0bedb8d9dc20469b0c624b6cbbdfa81e26a
Manish Tuladhar <manish.eclipse@gmail.com>
2020-06-12 12:14:21 +0545

    Added settings and menu

--- app/src/main/java/np/com/manishtuladhar/trymusicvisualizer/VisualizerActivity.java
+++ app/src/main/java/np/com/manishtuladhar/trymusicvisualizer/VisualizerActivity.java
@@ -58,6 +58,30 @@ public class VisualizerActivity extends AppCompatActivity {
             mAudioInputReader.restart();
         }
     }
+    /**
+     * Methods for setting up the menu
+     **/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
+        MenuInflater inflater = getMenuInflater();
+        /* Use the inflater's inflate method to inflate our visualizer_menu layout to this menu */
+        inflater.inflate(R.menu.visualizer_menu, menu);
+        /* Return true so that the visualizer_menu is displayed in the Toolbar */
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(startSettingsActivity);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
 
     /**
